b0b971ff4fb71675a6638f3821f852e2e5062483
jwillage <jwillage@gmail.com>
2015-07-31 16:13:56 -0400

    fix and cleanup looping. break out pre 9/14 file handling

--- cb_analysis.R
+++ cb_analysis.R
@@ -15,36 +15,56 @@ end <- ymd(paste(endYear, endMonth, "01"))
 
 months <- seq(start, end, by = "1 month")
 
-setwd("data/")
+#setwd("data/")
 
 dat <- data.table()
 stations <- data.table()
 
 #download each file, unzip and rbind it to dat, then remove file and zip
 #format of filename and dates changed starting 9/2014, so handle separately
+
+#need to aggregate each file as it's read in - not enough memory to read
+#full dataset
 for (m in 1:length(months)){
   dt <- paste0(year(months[m]), sprintf("%02d", month(months[m])))
   u <- paste0("https://s3.amazonaws.com/tripdata/", dt, 
               "-citibike-tripdata.zip")
-#  download.file(u, paste0(dt, ".zip"))
-#  unzip(paste0(dt, ".zip"))
-  
+
   if (months[m] < "2014-08-02"){ #old format
+    fil <- paste(year(months[m]), sprintf("%02d ", month(months[m])), 
+                " Citi Bike trip data.csv", sep = "-")
+    if (!file.exists(fil)){
+        download.file(u, paste0(dt, ".zip"))
+        unzip(paste0(dt, ".zip"))   
+        file.remove(paste0(dt, ".zip"))
+    }
+    
+    #Skip over the station name and geo coordinates for now to save space
     tmp <- fread(paste(year(months[m]), sprintf("%02d ", month(months[m])), 
                        " Citi Bike trip data.csv", sep = "-"), 
-                 select = c(1, 2, 3, 4, 8))
+                 select = c(1:4, 8, 12:15), na.strings = "\\N")
+                
     tmp$starttime <- ymd_hms(tmp$starttime)
     tmp$stoptime <- ymd_hms(tmp$stoptime)
     
+    #build station lookup table
     tmp.st <- fread(paste(year(months[m]), sprintf("%02d ", month(months[m])), 
                           " Citi Bike trip data.csv", sep = "-"), 
                     select = c(4, 5, 6, 7))
   }
   
   if (months[m] >= "2014-08-02"){ #new format
+    fil <- paste0(year(months[m]), sprintf("%02d", month(months[m])), 
+                  "-citibike-tripdata.csv")
+    if (!file.exists(fil)){
+      download.file(u, paste0(dt, ".zip"))
+      unzip(paste0(dt, ".zip"))   
+      file.remove(paste0(dt, ".zip"))
+    }
+    
     tmp <- fread(paste0(year(months[m]), sprintf("%02d", month(months[m])), 
                         "-citibike-tripdata.csv"), 
-                 select = c(1, 2, 3, 4, 8))
+                 select = c(1:4, 8, 12:15), na.strings = "\\N")
     tmp$starttime <- mdy_hms(tmp$starttime)
     tmp$stoptime <- mdy_hms(tmp$stoptime)
     tmp.st <- fread(paste0(year(months[m]), sprintf("%02d", month(months[m])), 
@@ -52,46 +72,20 @@ for (m in 1:length(months)){
                     select = c(4, 5, 6, 7))
   }
   
-  #dat <- rbind(dat, read.csv(paste0(list.files(pattern = "csv"))))
-  #TODO change to be less complcated
-  
-  #Skip over the station name and geo coordinates for now to save space
-#  dat <- rbind(dat, 
-#        fread(grep(paste0(".*", year(months[m]), ".*", month(months[m]), ".*"), 
-#             list.files(pattern="csv"), value =T), select=c(1, 2, 3, 4, 8)))
-
   dat <- rbind(dat, tmp)
-  stations <- rbind(stations, tmp.st)
+  stations <- unique(rbind(stations, tmp.st))
+  
     #danger
   #file.remove(list.files())
-  
-  #build station lookup table
-#  stations <- rbind(stations, 
-#               fread(grep(paste0(".*", year(months[m]), ".*", month(months[m]), ".*"), 
-#               list.files(pattern="csv"), value =T), select=c(4, 5, 6, 7)))
-#  stations <- unique(stations)
+
 }
 
+rm(list = c("tmp", "tmp.st"))
 setwd("..")
 
 setnames(dat, make.names(names(dat)))
 
-#start/stop times are in different formats
-dat$starttime <- sub(".*-.*", paste0(
-                    month(ymd_hms(dat$starttime)), "/", 
-                    day(ymd_hms(dat$starttime)), "/", 
-                    year(ymd_hms(dat$starttime)), " ", 
-                    hour(ymd_hms(dat$starttime)), ":", 
-                    minute(ymd_hms(dat$starttime))), dat$starttime)
 
-dat$stoptime <- sub(".*-.*", paste0(
-                    month(ymd_hms(dat$stoptime)), "/", 
-                    day(ymd_hms(dat$stoptime)), "/", 
-                    year(ymd_hms(dat$stoptime)), " ",
-                    hour(ymd_hms(dat$stoptime)), ":", 
-                    minute(ymd_hms(dat$stoptime))), dat$stoptime)
-
-dat$birth.year <- sub("\\N", NA, dat$birth.year)
 dat$birth.year <- as.numeric(dat$birth.year)
 dat$tripduration <- as.numeric(dat$tripduration)
 
