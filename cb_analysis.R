12c068a72a3da9d485bb7782f3c7535206ccfa1f
jwillage <jwillage@gmail.com>
2015-08-06 10:59:24 -0400

    further functionalized processMonths into trip and station functions

--- cb_analysis.R
+++ cb_analysis.R
@@ -32,10 +32,14 @@ stationDistanceX <- function(startLat, startLon, endLat, endLon){
   c(1, 2)
 }
 
-processMonth <- function(inMonth, inYear){
-  if (months[m] < "2014-08-02"){ #old format
-    fil <- paste0("data/", inYear, "-", 
-                  sprintf("%02d", inMonth), 
+getMonthData <- function(monthFile){
+  dt <- paste0(year(monthFile), sprintf("%02d", month(monthFile)))
+  u <- paste0("https://s3.amazonaws.com/tripdata/", dt, 
+              "-citibike-tripdata.zip")
+  
+  if (monthFile < "2014-08-02"){ #old format
+    fil <- paste0("data/", year(monthFile), "-", 
+                  sprintf("%02d", month(monthFile)), 
                   " - Citi Bike trip data.csv")
     
     if (!file.exists(fil)){
@@ -45,52 +49,57 @@ processMonth <- function(inMonth, inYear){
     }
     
     tmp <- fread(fil, na.strings = "\\N")
+  }
+  
+  if (monthFile >= "2014-08-02"){ #second through current format
+    fil <- paste0("data/", year(monthFile), sprintf("%02d", month(monthFile)), 
+                  "-citibike-tripdata.csv")
     
-    #Split into trip history and station lookup data
-    tmp.trip <- tmp[, c(1:4, 8, 12:15), with = FALSE]
-    tmp.station <- tmp[, c(4, 5, 6, 7), with = FALSE]
+    if (!file.exists(fil)){
+      download.file(u, paste0("data/", dt, ".zip"))
+      unzip(paste0("data/", dt, ".zip"))   
+      file.remove(paste0("data/", dt, ".zip"))
+    }
     
+    tmp <- fread(fil, na.strings = "\\N")
+  }
+  
+  #danger
+  #file.remove(...)
+  
+  tmp
+}
+
+processMonthTrip <- function(monthFile){
+  tmp <- getMonthData(monthFile)
+  
+  #Split into trip history 
+  tmp.trip <- tmp[, c(1:4, 8, 12:15), with = FALSE]
+  
+  if (monthFile < "2014-08-02"){ #old format
     tmp.trip$starttime <- ymd_hms(tmp$starttime)
     tmp.trip$stoptime <- ymd_hms(tmp$stoptime)
   }
   
-  if (months[m] >= "2014-08-02" & months[m] < "2015-05-31"){ #second format
-    fil <- paste0("data/", year(months[m]), sprintf("%02d", month(months[m])), 
-                  "-citibike-tripdata.csv")
-    
-    if (!file.exists(fil)){
-      download.file(u, paste0("data/", dt, ".zip"))
-      unzip(paste0("data/", dt, ".zip"))   
-      file.remove(paste0("data/", dt, ".zip"))
-    }
-    
-    tmp <- fread(fil, na.strings = "\\N")
-    tmp.trip <- tmp[, c(1:4, 8, 12:15), with = FALSE]
-    tmp.station <- tmp[, c(4, 5, 6, 7), with = FALSE]
-    
+  if (monthFile >= "2014-08-02" & monthFile < "2015-05-31"){ #second format
     tmp.trip$starttime <- mdy_hms(tmp$starttime)
     tmp.trip$stoptime <- mdy_hms(tmp$stoptime)
   }
   
-  if (months[m] > "2015-05-31"){ #current format
-    fil <- paste0("data/", year(months[m]), sprintf("%02d", month(months[m])), 
-                  "-citibike-tripdata.csv")
-    if (!file.exists(fil)){
-      download.file(u, paste0("data/", dt, ".zip"))
-      unzip(paste0("data/", dt, ".zip"))   
-      file.remove(paste0("data/", dt, ".zip"))
-    }
-    
-    tmp <- fread(fil, na.strings = "\\N")
-    tmp.trip <- tmp[, c(1:4, 8, 12:15), with = FALSE]
-    tmp.station <- tmp[, c(4, 5, 6, 7), with = FALSE]
-    
+  if (monthFile > "2015-05-31"){ #current format
     tmp.trip$starttime <- mdy_hm(tmp$starttime)
     tmp.trip$stoptime <- mdy_hm(tmp$stoptime)
   }
   
-  list(tmp.trip, tmp.station)
+  tmp.trip
+}
+
+processMonthStation <- function(monthFile){
+  tmp <- getMonthData(monthFile)
   
+  #Split into stations
+  tmp.station <- tmp[, c(4, 5, 6, 7), with = FALSE]
+  tmp.station
 }
 
 
@@ -110,58 +119,46 @@ months <- seq(start, end, by = "1 month")
 dat <- data.table()
 stations <- data.table()
 
+#process the most recent file to get the up-to-date station list
+stations <- processMonthStation(tail(months, 1))
+setnames(stations, make.names(names(stations)))
 
-for (m in 1:length(months)){
-  dt <- paste0(year(months[m]), sprintf("%02d", month(months[m])))
-  u <- paste0("https://s3.amazonaws.com/tripdata/", dt, 
-              "-citibike-tripdata.zip")
+#calculate all combinations of stations and the distance between them,
+#then map them back to each rider
+comb <- as.data.table(levels(interaction(paste(stations$start.station.id, 
+                                               stations$start.station.name,
+                                               stations$start.station.latitude, 
+                                               stations$start.station.longitude,
+                                               sep = ";"), 
+                                         paste(stations$start.station.id, 
+                                               stations$start.station.name,
+                                               stations$start.station.latitude, 
+                                               stations$start.station.longitude,
+                                               sep = ";")
+                                         , sep = ";")))
 
-  l <- processMonth(month(months[m]), year(months[m]))
-  
-  
-  dat <- rbind(dat, l[[1]])
-  stations <- unique(rbind(stations, l[[2]]))
-#  setnames(stations, make.names(names(stations)))
-  
-  #calculate all combinations of stations and the distance between them,
-  #then map them back to each rider
-  comb <- as.data.table(levels(interaction(paste(stations$`start station id`, 
-                                   stations$`start station name`,
-                                   stations$`start station latitude`, 
-                                   stations$`start station longitude`, sep = ";"), 
-                             paste(stations$`start station id`, 
-                                   stations$`start station name`,
-                                   stations$`start station latitude`, 
-                                   stations$`start station longitude`, sep = ";")
-                             , sep = ";")))
-  
-  comb <- separate(comb, V1, c("start.station.id", "start.station.name", "start.station.latitude" 
-                        ,"start.station.longitude" , "end.station.id" , "end.station.name" ,
-                        "end.station.latitude" ,
-                         "end.station.longitude"), sep = ";")
-  
-  #todo assign function results to multiple columns. Can this be done easily with <- notation?
-  #if not, return a list/vector of both results and then assign individually
-  #comb$distance <- 0; comb$estimatedDuration <- 0
-  #comb[, 9:10] <- with(comb, stationDistanceX(start.station.latitude, 
+comb <- separate(comb, V1, c(names(stations), 
+                             sub('start', 'end', names(stations))), sep = ";")
+
+#todo assign function results to multiple columns. 
+#Can this be done easily with <- notation?
+#if not, return a list/vector of both results and then assign individually
+#comb$distance <- 0; comb$estimatedDuration <- 0
+#comb[, 9:10] <- with(comb, stationDistanceX(start.station.latitude, 
 
 #  comb$distance <- with(comb, stationDistanceX(start.station.latitude, 
 #                                              start.station.longitude,
 #                                              end.station.latitude, 
 #                                              end.station.longitude))
-  
-  
-  #comb$estimatedDuration
-                          
-    #danger
-  #file.remove(list.files())
 
+#comb$estimatedDuration
+
+#get trip history for all months
+for (m in 1 : (length(months))){
+  dat <- rbind(dat, processMonthTrip(months[m]))
 }
 
-rm(list = c("tmp", "tmp.trip", "tmp.station"))
-
 setnames(dat, make.names(names(dat)))
-setnames(stations, make.names(names(stations)))
 
 dat$birth.year <- as.numeric(dat$birth.year)
 dat$tripduration <- as.numeric(dat$tripduration)
